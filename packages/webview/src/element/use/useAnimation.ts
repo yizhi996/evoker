066e4ae669d5abe068f2767248640fdf79ac9fc9
yizhi996 <dskcpp@gmail.com>
2022-03-18 17:08:18 +0800

    feat(webview): add useAnimation

---
+++ packages/webview/src/element/use/useAnimation.ts
@@ -0,0 +1,46 @@
+import { Tween, update } from "@tweenjs/tween.js"
+
+interface AnimationOptions<T> {
+  begin: T
+  end: T
+  duration: number
+  easing: (amount: number) => number
+  onUpdate: (value: T) => void
+  onComplete: () => void
+}
+
+export default function useAnimation<T>() {
+  let currentAnimation = 0
+  let currentTween: Tween<T> | undefined
+
+  const animate = () => {
+    currentAnimation = requestAnimationFrame(animate)
+    update()
+  }
+
+  const startAnimation = (options: AnimationOptions<T>) => {
+    currentAnimation = requestAnimationFrame(animate)
+    currentTween && currentTween.stop()
+
+    currentTween = new Tween(options.begin)
+      .to(options.end, options.duration)
+      .easing(options.easing)
+      .onUpdate(cb => {
+        options.onUpdate(cb)
+      })
+      .onComplete(() => {
+        cancelAnimationFrame(currentAnimation)
+        options.onComplete()
+      })
+      .start()
+  }
+
+  const stopAnimation = () => {
+    cancelAnimationFrame(currentAnimation)
+  }
+
+  return {
+    startAnimation,
+    stopAnimation
+  }
+}
