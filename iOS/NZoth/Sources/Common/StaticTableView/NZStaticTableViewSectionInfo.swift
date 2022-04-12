7c05585b805ee20925fab36c201846db7a6ead6a
yizhi996 <dskcpp@gmail.com>
2022-04-12 22:19:04 +0800

    feat(iOS): 新增 static tableView

---
+++ iOS/NZoth/Sources/Common/StaticTableView/NZStaticTableViewSectionInfo.swift
@@ -0,0 +1,62 @@
+//
+//  NZStaticTableViewSectionInfo.swift
+//
+//  Copyright (c) NZoth. All rights reserved. (https://nzothdev.com)
+//  
+//  This source code is licensed under The MIT license.
+//
+
+import Foundation
+import UIKit
+
+class NZStaticTableViewSectionInfo {
+    
+    private(set) var cells: [NZStaticTableViewCellInfo] = []
+    
+    var header: String?
+    
+    var footer: String?
+    
+    var headerHeight: CGFloat = 0
+    
+    var footerHeight: CGFloat = 0
+    
+    var makeHeaderViewHandler: ((NZStaticTableViewSectionInfo) -> UIView)?
+    
+    var makeFooterViewHandler: ((NZStaticTableViewSectionInfo) -> UIView)?
+    
+    convenience init(header: String) {
+        self.init()
+        self.header = header
+    }
+    
+    convenience init(footer: String) {
+        self.init()
+        self.footer = footer
+    }
+    
+    convenience init(header: String, footer: String) {
+        self.init()
+        self.header = header
+        self.footer = footer
+    }
+    
+}
+
+extension NZStaticTableViewSectionInfo: NZStaticTableViewListProtocol {
+    
+    typealias T = NZStaticTableViewCellInfo
+    
+    var count: Int {
+        return cells.count
+    }
+    
+    func append(_ info: T) {
+        cells.append(info)
+    }
+    
+    func remove(at index: Int) {
+        guard index < cells.count else { return }
+        cells.remove(at: index)
+    }
+}
