759f5cf45fb65258550fc795348308deb1f23aa9
yizhi996 <dskcpp@gmail.com>
2022-04-13 20:30:19 +0800

    fix(example): set title 设置失败

--- Examples/example/src/pages/API/SetTitle.vue
+++ Examples/example/src/pages/API/SetTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white flex items-center px-2.5 w-full mt-5 h-11">
     <div class="flex-shrink-0">页面标题</div>
-    <input class="ml-2.5" v-model="title" placeholder="请输入页面标题" />
+    <input class="ml-2.5" v-model:value="title" placeholder="请输入页面标题" />
   </div>
   <button type="primary" @click="onSetTitle">设置</button>
 </template>
