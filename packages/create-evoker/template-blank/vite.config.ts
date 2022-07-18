1ef1dbd8c05f3158d52436e56995631cc975f4ae
yizhi996 <dskcpp@gmail.com>
2022-07-18 22:19:23 +0800

    fix(create-evoker): 修复 vite 3.0 `process.env.*` 在 lib 模式不再被 replace 的问题

--- packages/create-evoker/template-blank/vite.config.ts
+++ packages/create-evoker/template-blank/vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite"
 import evoker from "@evoker/vite-plugin"
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode)
+    },
     plugins: [
       evoker({
         devtools: {
--- packages/create-evoker/template-example/vite.config.ts
+++ packages/create-evoker/template-example/vite.config.ts
@@ -3,8 +3,11 @@ import evoker from "@evoker/vite-plugin"
 import { resolve } from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode)
+    },
     resolve: {
       alias: {
         "@": resolve(__dirname, "./src")
