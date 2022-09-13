45537a0615a31a2a013615ea4ccfdbcadae60b09
yizhi996 <dskcpp@gmail.com>
2022-09-13 15:19:38 +0800

    fix(template): example 的 NObject 组件传入 string 时显示错误

--- packages/create-evoker/template-example/src/components/NObject.vue
+++ packages/create-evoker/template-example/src/components/NObject.vue
@@ -4,7 +4,7 @@
       <template v-for="item of sorted" :key="item.key">
         <div class="text-blue-400">
           {{ item.key }}:
-          <span class="text-orange-400 ml-1">{{ item.value }}</span>
+          <span class="text-orange-400 ml-1 break-all">{{ item.value }}</span>
         </div>
       </template>
     </template>
@@ -14,13 +14,18 @@
 
 <script setup lang="ts">
 import { computed } from "vue"
+import { isString } from "@vue/shared"
 
 const props = defineProps<{ object: Record<string, any>; placeholder?: string }>()
 
 const sorted = computed(() => {
-  let res = []
   const obj = props.object
-  for (let key in obj) {
+  if (isString(obj)) {
+    return [{ key: "body", value: obj }]
+  }
+
+  const res = []
+  for (const key in obj) {
     res.push({ key, value: obj[key] })
   }
 
